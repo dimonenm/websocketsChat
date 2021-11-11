b14b20f6fede8fbc3bfb92e35925af5e9175a0ed
arzuakov@yahoo.com <arzuakov@yahoo.com>
2021-11-11 17:55:33 +0300

    w11.11.2021

--- server/websocket.js
+++ server/websocket.js
@@ -1,2 +1,31 @@
 const ws = require('ws');
 
+const wss = new ws.Server({
+    port: 4000,
+}, () => {
+    console.log('Server started on 4000');
+});
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function (message) {
+        message = JSON.parse(message);
+
+        switch (message.event) {
+            case 'message':
+                broadcastMessage(message);
+                break;
+            case 'connection':
+                broadcastMessage(message);
+                break;
+            default:
+                break;
+        }
+    })
+})
+
+function broadcastMessage(message) {
+    wss.clients.forEach(client => {
+        client.send(JSON.stringify(message));
+    })
+}
+
