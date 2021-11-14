a84ad3932dddf2a0a50fade91c1a9a23010c1c0a
exp <arzuakov@yahoo.com>
2021-11-15 02:30:51 +0300

    h15.11.2021

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import { Button, DatePicker, Row, Col, Divider } from "antd";
+import { Button, DatePicker, Layout, Row, Col, Divider } from "antd";
 import './App.css';
 import "antd/dist/antd.css";
 
 function App() {
   const style = { background: '#0092ff', padding: '8px 0' };
+  const { Header, Footer, Content } = Layout;
   return (
     <div className="App">
-      <Divider orientation="center">Horizontal</Divider>
-      <Row gutter={16} justify="center">
-        <Col className='gutter-row' span={3}>
-          <DatePicker />
-        </Col>
-        <Col className='gutter-row' span={3}>
-          <Button type="primary">
-          Primary Button
-          </Button>
-        </Col>
-      </Row>
-      
+      <Layout style={{height: '100%'}}>
+        <Header>Header</Header>
+        <Content>
+          <Layout>
+            <Row gutter={16} justify="center">
+              <Col span={3}>
+                <DatePicker />
+              </Col>
+              <Col span={3}>
+                <Button type="primary">
+                  Primary Button
+                </Button>
+              </Col>
+            </Row>
+          </Layout>
+        </Content>
+        <Footer />
+      </Layout>
     </div>
   );
 }
