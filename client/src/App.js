5fcb75f5205b4625104153ecc2452ab96d5336c7
arzuakov@yahoo.com <arzuakov@yahoo.com>
2021-11-15 17:20:35 +0300

    w15.11.2021

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,28 @@
-import { Button, DatePicker, Layout, Row, Col, Divider } from "antd";
-import './App.css';
-import "antd/dist/antd.css";
+import { Button, DatePicker, Layout, Row, Col } from "antd";
+import './App.css'; // ?
 
 function App() {
-  const style = { background: '#0092ff', padding: '8px 0' };
-  const { Header, Footer, Content } = Layout;
+  const { Header, Footer, Content, Sider } = Layout;
   return (
-    <div className="App">
-      <Layout style={{height: '100%'}}>
+    <Layout>
+      <Sider>
+
+      </Sider>
+      <Layout>
         <Header>Header</Header>
-        <Content>
-          <Layout>
-            <Row gutter={16} justify="center">
-              <Col span={3}>
-                <DatePicker />
-              </Col>
-              <Col span={3}>
-                <Button type="primary">
-                  Primary Button
-                </Button>
-              </Col>
-            </Row>
-          </Layout>
-        </Content>
-        <Footer />
+        <Content style={{overflow: 'auto'}}></Content>
+        <Footer>
+          <Row>
+            <Col span={10} offset={1}>
+              <DatePicker />
+            </Col>
+            <Col span={10} offset={1}>
+              <Button>Ввести</Button>
+            </Col>
+          </Row>
+        </Footer>
       </Layout>
-    </div>
+    </Layout>
   );
 }
 
